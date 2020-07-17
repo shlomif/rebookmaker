505d1dcc69a5543baa795b6cb37caff660c43405
Shlomi Fish <shlomif@shlomifish.org>
2020-07-17 15:11:45 +0300

    wip: convert to FLOSS epub maker

--- shlomif_epub_maker.py
+++ shlomif_epub_maker.py
@@ -6,8 +6,19 @@
 #
 # Distributed under the MIT license.
 
+xmlns1 = "urn:oasis:names:tc:opendocument:xmlns:container"
+medtype1 = "application/oebps-package+xml"
+EPUB_CONTAINER = ('''<?xml version="1.0"?>
+<container version="1.0" xmlns="{xmlns1}">
+    <rootfiles>
+        <rootfile full-path="OEBPS/content.opf" media-type="{medtype1}"/>
+    </rootfiles>
+</container>''').format(medtype1=medtype1, xmlns1=xmlns1)
+
+
 def _my_amend_epub(filename):
     from zipfile import ZipFile, ZIP_STORED
     z = ZipFile(filename, 'a')
     z.writestr("mimetype", "application/epub+zip", ZIP_STORED)
+    z.writestr("META-INF/container.xml", EPUB_CONTAINER, ZIP_STORED)
     z.close()
