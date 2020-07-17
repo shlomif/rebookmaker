ff1af1850161fa1474c66f46ce2d61732e566f6f
Shlomi Fish <shlomif@shlomifish.org>
2020-07-17 20:49:27 +0300

    [WiP] convert to a FLOSS epub maker (licensing)

--- shlomif_epub_maker.py
+++ shlomif_epub_maker.py
@@ -149,4 +149,11 @@ def _my_amend_epub(filename, json_fn):
     )
     content_text = re.sub("[\\n\\r]*\\Z", "\n", content_text)
     z.writestr("OEBPS/content.opf", content_text, ZIP_STORED)
+    template = env.get_template('tox-ncx' + '.jinja')
+    content_text = template.render(
+        author_name=j['authors'][0]['name'],
+        title=j['title'],
+    )
+    content_text = re.sub("[\\n\\r]*\\Z", "\n", content_text)
+    z.writestr("OEBPS/toc.ncx", content_text, ZIP_STORED)
     z.close()
